0abd25620ea349f78ac7723f27e5666ae9d30bda
liukaiye <116006594+Lkyaaa@users.noreply.github.com>
2022-10-23 16:58:47 +0800

    Update main.c

--- task1/main.c
+++ task1/main.c
@@ -1,8 +1,26 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+int main(void) {
+	double x0=0;
+	float a;
+	float h=0.001;
+	int n=-1;
+	int k;
 
-int main(int argc, char *argv[]) {
-	printf("Hello World!\n");
+    
+	printf("Please enter a primary value.(value of \"a\") \n");
+	scanf("%f",&a);
+	printf("Please enter the number of points.\n");
+	scanf("%d",&k);
+	printf("n            xn             yn\n");
+	while(n++<k) {
+		printf("%-3d%15f%15f\n",n,x0,a);
+		x0=x0+h;
+		a=(1-h)*a;
+
+
+
+	}
+	
+
 	return 0;
 }
-
